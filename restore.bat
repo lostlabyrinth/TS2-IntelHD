c1cc17c0e2f348a19668b7de2d551c7eb097fce2
lostlabyrinth <lab@lostlabyrinth.co.uk>
2014-07-17 01:25:04 +0100

    add restore file

---
+++ restore.bat
@@ -0,0 +1,5 @@
+move "C:\Program Files (x86)\Origin Games\The Sims 2 Ultimate Collection\Fun with Pets
+
+\SP9\TSData\Res\Config\Graphics Rules.sgr.bak" "C:\Program Files (x86)\Origin Games\The Sims 2 
+
+Ultimate Collection\Fun with Pets\SP9\TSData\Res\Config\Graphics Rules.sgr"
