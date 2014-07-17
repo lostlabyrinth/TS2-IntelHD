8672e37dad6b6ee8ceec5af2aa57551b7336c39e
lostlabyrinth <lab@lostlabyrinth.co.uk>
2014-07-17 00:46:05 +0000

    add experimental 4K installers

---
+++ 4K_README.txt
@@ -0,0 +1,4 @@
+TS2-IntelHD 4K Readme
+======================
+
+The 4K installer allows for the game to use higher resolutions such as 4K. This is currently experimental, hence why it is not part of the main file. To use it, simply run the 4K installer.
---
+++ 4K_install.bat
@@ -0,0 +1,11 @@
+move "C:\Program Files (x86)\Origin Games\The Sims 2 Ultimate Collection\Fun with Pets
+
+\SP9\TSData\Res\Config\Graphics Rules.sgr" "C:\Program Files (x86)\Origin Games\The Sims 2
+
+Ultimate Collection\Fun with Pets\SP9\TSData\Res\Config\Graphics Rules.sgr.bak"
+
+move "C:\Program Files (x86)\Origin Games\The Sims 2 Ultimate Collection\Fun with Pets
+
+\SP9\TSData\Res\Config\Graphics Rules_4K.sgr" "C:\Program Files (x86)\Origin Games\The Sims 2 
+
+Ultimate Collection\Fun with Pets\SP9\TSData\Res\Config\Graphics Rules.sgr"
